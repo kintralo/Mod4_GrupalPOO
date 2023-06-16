75484ee4295fa2962397117e96f02a4f28779a22
Pablov <juan.vasquez@valposystem.com>
2023-06-15 21:06:32 -0400

    Creación de atributos de Usuarios

--- ejercicioGrurpal_13/src/models/Usuario.java
+++ ejercicioGrurpal_13/src/models/Usuario.java
@@ -1,4 +1,54 @@
 package models;
 
+import java.util.Date;
+
+/***
+ * @author Juan Pablo Vásquez
+ */
 public class Usuario {
+    String Nombre;
+    Date Fecha_de_nacimiento;
+    String RUN;
+
+    public Usuario() {
+    }
+
+    public Usuario(String nombre, Date fecha_de_nacimiento, String RUN) {
+        Nombre = nombre;
+        Fecha_de_nacimiento = fecha_de_nacimiento;
+        this.RUN = RUN;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public Date getFecha_de_nacimiento() {
+        return Fecha_de_nacimiento;
+    }
+
+    public void setFecha_de_nacimiento(Date fecha_de_nacimiento) {
+        Fecha_de_nacimiento = fecha_de_nacimiento;
+    }
+
+    public String getRUN() {
+        return RUN;
+    }
+
+    public void setRUN(String RUN) {
+        this.RUN = RUN;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "Nombre='" + Nombre + '\'' +
+                ", Fecha_de_nacimiento=" + Fecha_de_nacimiento +
+                ", RUN='" + RUN + '\'' +
+                '}';
+    }
 }
