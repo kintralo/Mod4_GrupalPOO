bac77faec6ec7b747f179d910b2dd04b823f6633
kintralo <aotm2000@gmail.com>
2023-06-16 01:38:49 -0400

    Se crea atributo para clase Cliente

--- ejercicioGrurpal_13/src/models/Cliente.java
+++ ejercicioGrurpal_13/src/models/Cliente.java
@@ -1,4 +1,12 @@
 package models;
 
 public class Cliente {
+    Integer RUT;
+    Integer Edad;
+    String Nombres;
+    String Apellido;
+    String Telefono;
+    String AFP;
+    String Direccion;
+    String Comuna;
 }
