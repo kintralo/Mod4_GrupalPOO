c91bcb1c1e9b13af4347d65da539267b2ec56e79
Pablov <juan.vasquez@valposystem.com>
2023-06-15 22:10:06 -0400

    Creación de objetos en Clase main

--- ejercicioGrurpal_13/src/Main.java
+++ ejercicioGrurpal_13/src/Main.java
@@ -1,12 +1,28 @@
 import models.Usuario;
+import models.Cliente;
+import models.Capacitacion;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Usuario usuario = new Usuario("María", '01-01-1990', "87654321");
+        Cliente cliente = new Cliente(12345678, "Juan", "Pérez", "912345678", "AFP Modelo", 1, "Calle 123", "Santiago", 30);
+        Usuario usuario = new Usuario("María", "01-01-1990", 87654321);
+        Capacitacion capacitacion = new Capacitacion(1, 12345678, "01-01-2023", "09:00", "Centro de Convenciones", 3, 50);
+
+        System.out.println("Datos del cliente:");
+        System.out.println(cliente.toString());
+        System.out.println();
 
         System.out.println("Datos del usuario:");
         System.out.println(usuario.toString());
+        System.out.println();
+
+        System.out.println("Datos de la capacitación:");
+        System.out.println(capacitacion.toString());
         System.out.println();
     }
 }
